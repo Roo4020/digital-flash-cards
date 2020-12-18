75a093330011812e5c7b9836a8565bd3c8227bc8
sinowin <sinowin4020@gamil.com>
2020-12-18 22:19:24 +0900

    サイドバー実装

---
+++ src/components/layouts/GlobalAside.vue
@@ -0,0 +1,169 @@
+<template>
+  <div :class="['aside-wrapper', setClassShow]" @click.self="shiftAside">
+    <div class="aside">
+      <div class="aside-header">
+        <div class="aside-header-cross" @click="shiftAside">
+          <img src="@/assets/icon/aside_close.svg" />
+        </div>
+      </div>
+      <div class="aside-content">
+        <div class="aside-content-item">
+          <div class="aside-content-item-tab">単語帳</div>
+          <div class="aside-content-item-btn" @click="transFlashCards(0)">
+            独→日
+          </div>
+          <div class="aside-content-item-btn" @click="transFlashCards(1)">
+            日→独
+          </div>
+        </div>
+        <div class="aside-content-item">
+          <div class="aside-content-item-tab">編集</div>
+          <div class="aside-content-item-btn" @click="transSearch">検索</div>
+          <div class="aside-content-item-btn" @click="transAppend">追加</div>
+        </div>
+      </div>
+      <div class="aside-footer">
+        <div class="aside-footer-btn" @click="transTop">トップへ戻る</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { ASIDE_MENU_LIST } from "@/mixins/asideMenuList.js";
+
+export default {
+  name: "GlobalAside",
+  data() {
+    return {
+      // asideMenuList: ASIDE_MENU_LIST,
+    };
+  },
+  computed: {
+    showAside() {
+      return this.$store.state.common.showAside;
+    },
+    setClassShow() {
+      return this.showAside ? "show" : "";
+    },
+  },
+  methods: {
+    transFlashCards(key) {
+      this.shiftAside();
+      this.$store.commit("changeTranslate", key);
+      this.$router.push("/translate");
+    },
+    transSearch() {
+      this.shiftAside();
+      this.$router.push("/search");
+    },
+    transAppend() {
+      this.shiftAside();
+      this.$router.push("/append");
+    },
+    transTop() {
+      this.shiftAside();
+      this.$router.push("/");
+    },
+    shiftAside() {
+      this.$store.commit("shiftAside");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.aside-wrapper {
+  z-index: 2;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.6);
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  transform: translateX(100%);
+  .aside {
+    width: 200px;
+    height: 100%;
+    background: rgb(244, 244, 244);
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translateX(200px);
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    transition: all 0.4s ease;
+    &-header {
+      width: 100%;
+      height: 72px;
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      &-cross {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-right: 16px;
+        padding: 4px;
+        cursor: pointer;
+        img {
+          transform: scale(1.6, 1.6);
+        }
+        &:hover {
+          border: 1px solid black;
+        }
+      }
+    }
+
+    &-content {
+      width: 100%;
+      padding: 16px;
+      &-item {
+        width: 100%;
+        margin: 8px 0px;
+        &-tab {
+          width: 100%;
+          font-size: 16px;
+          font-weight: bold;
+          border-bottom: 1px solid black;
+          padding-left: 16px;
+        }
+        &-btn {
+          width: 80%;
+          font-size: 20px;
+          text-align: center;
+          text-decoration: underline;
+          cursor: pointer;
+          &:hover {
+            color: rgb(103, 103, 103);
+          }
+        }
+      }
+    }
+
+    &-footer {
+      width: 100%;
+      padding: 16px;
+      &-btn {
+        width: 80%;
+        font-size: 16px;
+        text-align: center;
+        text-decoration: underline;
+        cursor: pointer;
+        &:hover {
+          color: rgb(103, 103, 103);
+        }
+      }
+    }
+  }
+}
+
+.show {
+  transform: translateX(0) !important;
+  div {
+    transform: translateX(0) !important;
+  }
+}
+</style>
--- src/components/layouts/GlobalHeader.vue
+++ src/components/layouts/GlobalHeader.vue
@@ -1,16 +1,8 @@
 <template>
   <div class="global-header">
     <HeaderLogo />
-    <div class="menu">
-      <div :class="['menu-box', setClassOpen]">
-        <div class="menu-box-content" @click="transFlashCards">単語帳</div>
-        <div class="menu-box-content" @click="transSearch">検索</div>
-        <div class="menu-box-content" @click="transAppend">追加</div>
-        <div class="menu-box-content" @click="transTop">トップへ</div>
-      </div>
-      <div class="menu-shift" @click="shiftBoxSize">
-        <img :src="require(`@/assets/icon/${ iconName }.svg`)" />
-      </div>
+    <div class="aside-handle" @click="shiftAside">
+      <img src="@/assets/icon/aside_open.svg" />
     </div>
   </div>
 </template>
@@ -23,43 +15,14 @@ export default {
   components: {
     HeaderLogo,
   },
-  data() {
-    return {
-      openMenu: false,
-    };
-  },
   computed: {
-    translate() {
-      return this.$store.state.vocabulary.translate;
+    showAside() {
+      return this.$store.state.common.showAside;
     },
-    setClassOpen() {
-      return this.openMenu ? "open" : "";
-    },
-    iconName() {
-      return this.openMenu ? "menu_close" : "menu_open";
-    }
   },
   methods: {
-    transFlashCards() {
-      if (this.$route.path === "/translate") {
-        let key = this.translate === 0 ? 1 : 0;
-        this.$store.commit("changeTranslate", key);
-      } else {
-        this.$router.push("/translate");
-      }
-    },
-    transSearch() {
-      this.$router.push("/search");
-    },
-    transAppend() {
-      this.$router.push("/append");
-    },
-    transTop() {
-      this.$router.push("/");
-    },
-    shiftBoxSize() {
-      this.openMenu = !this.openMenu;
-      console.log(this.openMenu);
+    shiftAside() {
+      this.$store.commit("shiftAside");
     },
   },
 };
@@ -79,47 +42,12 @@ export default {
   border-bottom: 1px solid white;
   padding: 0px 16px;
 
-  .menu {
-    color: white;
+  .aside-handle {
+    transform: scale(2, 2);
     display: flex;
-    &-box {
-      width: 0px;
-      height: 0px;
-      display: flex;
-      justify-content: space-around;
-      align-items: center;
-      margin-right: 4px;
-      overflow: hidden;
-      transition: all 0.4s ease;
-      &-content {
-        font-size: 16px;
-        text-decoration: underline;
-        margin: 0px 4px;
-        cursor: pointer;
-        &:hover {
-          color: silver;
-        }
-      }
-    }
-    .open {
-      width: 216px !important;
-      height: auto !important;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-    }
-
-    &-shift {
-      transform: scale(1.5, 1.5);
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      border: 1px solid white;
-      padding: 2px;
-      cursor: pointer;
-      &:hover {
-        border-color: silver !important;
-      }
-    }
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
   }
 }
 </style>
